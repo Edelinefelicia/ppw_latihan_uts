2a71f475531c966b8e505cf1a6f0573c94b14006
Edelinefelicia <edelinedharmawan2906@gmail.com>
2024-10-07 17:20:52 +0700

    add bug

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@create')->name('index');
 
 Route::get('/profile', 'ProfileController@index')->name('profile');
 Route::put('/profile', 'ProfileController@update')->name('profile.update');
